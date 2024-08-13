bbb827acb9a0233fd4a94c3e932584ea86ddc67c
Dom Sutcliffe <115641458+domducsutcliffe@users.noreply.github.com>
2024-08-13 14:15:40 +0100

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket         	   = "vostock-backend-123"
+    bucket         	   = "vostock-backend-1234ignoreme"
     key                 = "state/terraform.tfstate"
     region         	   = "eu-west-2"
     encrypt        	   = true
