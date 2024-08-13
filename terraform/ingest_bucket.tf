36026a5968d01719059ebae50cdd0fb0a027fa27
domducsutcliffe <dom.sutcliffe@crowncommercial.gov.uk>
2024-08-13 14:20:09 +0100

    Removing dummy src, test and terraform

--- terraform/ingest_bucket.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_s3_bucket" "ingest_bucket" {
-    
-  tags = {
-    bucket = "ingestion-bucket-13082024"
-    Name        = "ingest-bucket"
-    Environment = "Dev"
-  }
-}
--- test/test_app.py
+++
@@ -1,5 +0,0 @@
-from src.app import hello_world
-
-def test_check_hello_world():
-    response = hello_world()
-    assert response == 'Hello world'
