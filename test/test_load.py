e73e349b5443017d3db2a27557a8605bb0cdc374
Thambi <thambi@Piraviens-MacBook-Pro.local>
2024-08-27 11:55:09 +0100

    tests for some util funcs

--- test/test_load.py
+++ test/test_load.py
@@ -4,7 +4,7 @@ import boto3
 import pytest
 import unittest
 from unittest.mock import patch, MagicMock
-from src.load import get_connection, get_secrets
+from src.load import get_connection, get_fact_purchase_order_query, get_fact_payment_query, get_dim_transaction_query, get_dim_transaction_query, get_fact_sales_order_query, get_secrets, get_dim_design_query, get_dim_staff_query, get_dim_location_query, get_dim_currency_query, get_dim_counterparty_query, get_dim_date_query
 
 
 @pytest.fixture(scope="function")
@@ -76,3 +76,193 @@ class TestConnection:
             user="wh_user",
             password="wh_password",
         )
+
+def test_get_dim_design_query():
+    test_query = """
+        INSERT INTO dim_design (
+            design_id,
+            design_name,
+            file_location,
+            file_name
+        )
+        VALUES (%s, %s, %s, %s)
+    """
+    test_func = get_dim_design_query()
+
+    assert test_func == test_query
+
+def test_get_dim_staff_query():
+    test_query = """
+        INSERT INTO dim_staff (
+            staff_id,
+            first_name,
+            last_name,
+            department_name,
+            location,
+            email_address
+        )
+        VALUES (%s, %s, %s, %s, %s, %s)
+    """
+    test_func = get_dim_staff_query()
+
+    assert test_func == test_query
+
+def test_get_dim_location_query():
+    test_query = """
+        INSERT INTO dim_location (
+            location_id,
+            address_line_1,
+            address_line_2,
+            district,
+            city,
+            postal_code,
+            country,
+            phone
+        )
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+    """
+    test_func = get_dim_location_query()
+
+    assert test_func == test_query
+
+def test_get_dim_currency_query():
+    test_query = """
+        INSERT INTO dim_currency (
+            currency_id,
+            currency_code,
+            currency_name
+        )
+        VALUES (%s, %s, %s)
+    """
+    test_func = get_dim_currency_query()
+
+    assert test_func == test_query
+
+def test_get_dim_counterparty_query():
+    test_query = """
+        INSERT INTO dim_counterparty (
+            counterparty_id,
+            counterparty_legal_name,
+            counterparty_legal_address_line_1,
+            counterparty_legal_address_line_2,
+            counterparty_legal_district,
+            counterparty_legal_city,
+            counterparty_legal_postal_code,
+            counterparty_legal_country,
+            counterparty_legal_phone_number
+        )
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+    """
+    test_func = get_dim_counterparty_query()
+
+    assert test_func == test_query
+
+def test_get_dim_date_query():
+    test_query = """
+        INSERT INTO dim_date (
+            date_id,
+            year,
+            month,
+            day,
+            day_of_week,
+            day_name,
+            month_name,
+            quarter
+        )
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+    """
+    test_func = get_dim_date_query()
+
+    assert test_func == test_query
+
+def test_get_fact_sales_order_query():
+    test_query = """
+        INSERT INTO fact_sales_order (
+            sales_order_id,
+            created_date,
+            created_time,
+            last_updated_date,
+            last_updated_time,
+            sales_staff_id,
+            counterparty_id,
+            units_sold,
+            unit_price,
+            currency_id,
+            design_id,
+            agreed_payment_date,
+            agreed_delivery_date,
+            agreed_delivery_location_id
+        )
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+    """
+    test_func = get_fact_sales_order_query()
+    assert test_func == test_query
+
+def test_get_dim_transaction_query():
+    test_query = """
+        INSERT INTO dim_transaction (
+            transaction_id,
+            transaction_type,
+            sales_order_id,
+            purchase_order_id
+        )
+        VALUES (%s, %s, %s, %s)
+    """
+    test_func = get_dim_transaction_query()
+    assert test_func == test_query
+
+
+def test_get_dim_payment_type_query():
+    test_query = """
+        INSERT INTO dim_payment_type (
+            payment_type_id,
+            payment_type_name
+        )
+        VALUES (%s, %s)
+    """
+    test_func = get_dim_transaction_query()
+    assert test_func == test_query
+
+def test_get_fact_payment_query():
+    test_query = """
+        INSERT INTO fact_payment (
+            payment_id,
+            created_date,
+            created_time,
+            last_updated_date,
+            last_updated_time,
+            transaction_id,
+            counterparty_id,
+            payment_amount,
+            currency_id,
+            payment_type_id,
+            paid,
+            payment_date
+        )
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+    """
+    test_func = get_fact_payment_query()
+    assert test_func == test_query
+
+def test_get_fact_purchase_order_query():
+    test_query = """
+        INSERT INTO fact_purchase_order (
+            purchase_order_id,
+            created_date,
+            created_time,
+            last_updated_date,
+            last_updated_time,
+            staff_id,
+            counterparty_id,
+            item_code,
+            item_quantity,
+            item_unit_price,
+            currency_id,
+            agreed_delivery_date,
+            agreed_payment_date,
+            agreed_delivery_location_id
+        )
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+    """
+    test_func = get_fact_purchase_order_query()
+    assert test_func == test_query
